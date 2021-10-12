b3ef02ee4106377c72bc0e758aa9de279aa768c7
rryaboshtan <rrvworkroman@gmail.com>
2021-10-12 19:08:44 +0300

    Add not-found-page component with angular schematics

--- src/app/routing/website/routing/not-found/not-found.module.ts
+++ src/app/routing/website/routing/not-found/not-found.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { NotFoundRoutingModule } from './not-found-routing.module';
-import { NotFoundComponent } from './not-found.component';
-
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    NotFoundComponent
+    NotFoundPageComponent
   ],
   imports: [
-    CommonModule,
-    NotFoundRoutingModule
+    CommonModule
   ]
 })
 export class NotFoundModule { }
--- src/app/routing/website/routing/not-found/pages/not-found-page/not-found-page.component.spec.ts
+++ src/app/routing/website/routing/not-found/pages/not-found-page/not-found-page.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NotFoundComponent } from './not-found.component';
+import { NotFoundPageComponent } from './not-found-page.component';
 
-describe('NotFoundComponent', () => {
-  let component: NotFoundComponent;
-  let fixture: ComponentFixture<NotFoundComponent>;
+describe('NotFoundPageComponent', () => {
+  let component: NotFoundPageComponent;
+  let fixture: ComponentFixture<NotFoundPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NotFoundComponent ]
+      declarations: [ NotFoundPageComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NotFoundComponent);
+    fixture = TestBed.createComponent(NotFoundPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
