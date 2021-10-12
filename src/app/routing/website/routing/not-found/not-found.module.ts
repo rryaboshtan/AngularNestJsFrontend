2b3eccdc964d86c27b5fb08524958ae7b6468c99
rryaboshtan <rrvworkroman@gmail.com>
2021-10-12 19:16:18 +0300

    Fix app-routing path to not-found module

--- src/app/routing/website/routing/not-found/not-found.module.ts
+++ src/app/routing/website/routing/not-found/not-found.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    NotFoundPageComponent
-  ],
+  declarations: [NotFoundPageComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '**',
+        component: NotFoundPageComponent,
+      },
+    ]),
+  ],
 })
-export class NotFoundModule { }
+export class NotFoundModule {}
