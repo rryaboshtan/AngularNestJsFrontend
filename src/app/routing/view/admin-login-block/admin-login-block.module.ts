f35ba75afa0d91d3647cf5a629e3129b0f404855
rryaboshtan <rrvworkroman@gmail.com>
2021-10-16 03:30:21 +0300

    login form design

--- src/app/routing/view/admin-login-block/admin-login-block.module.ts
+++ src/app/routing/view/admin-login-block/admin-login-block.module.ts
@@ -1,25 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
 import { AdminLoginBlockComponent } from './blocks/admin-login-block/admin-login-block.component';
 import { AdminLoginFormUiComponent } from './ui/admin-login-form-ui/admin-login-form-ui.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
-const routes: Routes = [
-  { path: '', component: AdminLoginBlockComponent }
-];
 
 @NgModule({
-  declarations: [
-    AdminLoginBlockComponent,
-    AdminLoginFormUiComponent
-  ],
+  declarations: [AdminLoginBlockComponent, AdminLoginFormUiComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
   ],
-  exports: [
-    AdminLoginBlockComponent
-  ]
+  exports: [AdminLoginBlockComponent],
 })
-export class AdminLoginBlockModule { }
+export class AdminLoginBlockModule {}
