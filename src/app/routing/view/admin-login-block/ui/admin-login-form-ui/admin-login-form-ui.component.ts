00585009d7f7d8203552811d11570f4dbf94042d
rryaboshtan <rrvworkroman@gmail.com>
2021-10-18 20:24:50 +0300

    add FormGroup

--- src/app/routing/view/admin-login-block/ui/admin-login-form-ui/admin-login-form-ui.component.ts
+++ src/app/routing/view/admin-login-block/ui/admin-login-form-ui/admin-login-form-ui.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-login-form-ui',
   templateUrl: './admin-login-form-ui.component.html',
-  styleUrls: ['./admin-login-form-ui.component.scss']
+  styleUrls: ['./admin-login-form-ui.component.scss'],
 })
 export class AdminLoginFormUiComponent implements OnInit {
+  formGroup:FormGroup = new FormGroup({});
 
-  constructor() { }
+  // @Input() formError = 'Foo';
+
+  constructor() {}
 
   ngOnInit(): void {
-  }
 
+    this.formGroup = new FormGroup({
+      login: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
 }
