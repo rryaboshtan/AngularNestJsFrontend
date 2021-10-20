10fdf90186f12d55662864c50848014b79bb511e
rryaboshtan <rrvworkroman@gmail.com>
2021-10-20 23:08:33 +0300

    Add pipe and map to the effect

--- src/app/store/admin-auth-store/store/admin-auth.effects.ts
+++ src/app/store/admin-auth-store/store/admin-auth.effects.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
-import { map, switchMap, catchError } from 'rxjs/operators';
+import { map, switchMap} from 'rxjs/operators';
+import { AdminAuthService } from '../services/admin-auth.service';
 import { login, loginSuccess } from './admin-auth.actions';
 
 @Injectable()
@@ -9,11 +10,16 @@ export class AdminAuthEffects {
   login$ = createEffect(() =>
     this.actions$.pipe(
       ofType(login),
-        switchMap((action) => of(loginSuccess({
-            accessToken: 'test'
-        })))
-    )
-  );
+        switchMap((action) => this.adminAuthService.login({
+            login: action.login,
+            password: action.password,
+        }).pipe(
+            map(loginSuccessData => loginSuccess(loginSuccessData))
+        ))
+    ));
 
-  constructor(private actions$: Actions) {}
+    constructor(
+        private actions$: Actions,
+        private adminAuthService: AdminAuthService,
+    ) { }
 }
