19f1ccb48cdf03a0e1b32a32ec47343c20d47ed7
rryaboshtan <rrvworkroman@gmail.com>
2021-10-20 22:16:47 +0300

    Add actions to the auth reducer

--- src/app/store/admin-auth-store/store/admin-auth.reducer.ts
+++ src/app/store/admin-auth-store/store/admin-auth.reducer.ts
@@ -1,4 +1,5 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { login, loginFailed, loginSuccess } from './admin-auth.actions';
 
 export const ADMIN_AUTH_FEATURE_NAME = 'admin-auth';
 
@@ -10,7 +11,7 @@ export interface AdminAuthState {
   loading: boolean;
   loaded: boolean;
   serverError: string;
-  authData?: AuthData;
+  authData?: AuthData | null;
 }
 
 const initialState: AdminAuthState = {
@@ -19,4 +20,24 @@ const initialState: AdminAuthState = {
   serverError: 'FOOO',
 };
 
-export const adminAuthReducer = createReducer(initialState);
+export const adminAuthReducer = createReducer(
+  initialState,
+  on(login, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(loginSuccess, (state, authData: AuthData) => ({
+    ...state,
+    authData,
+    loaded: true,
+    loading: false,
+    serverError: '',
+  })),
+  on(loginFailed, (state, { serverError }) => ({
+    ...state,
+    authData: null,
+    loaded: true,
+    loading: false,
+    serverError,
+  }))
+);
