ef0b1c7ec133cf08ea2155a10f4901ce6b06e70e
rryaboshtan <rrvworkroman@gmail.com>
2021-10-20 21:12:50 +0300

    Add admin auth selectors

---
+++ src/app/store/admin-auth-store/store/admin-auth.selectors.ts
@@ -0,0 +1,30 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AdminAuthState, ADMIN_AUTH_FEATURE_NAME } from './admin-auth.reducer';
+
+const getFeature = createFeatureSelector<AdminAuthState>(
+  ADMIN_AUTH_FEATURE_NAME
+);
+
+export const getLoading = createSelector(getFeature, (state) => state.loading);
+
+export const getLoaded = createSelector(getFeature, (state) => state.loaded);
+
+export const getServerError = createSelector(
+  getFeature,
+  (state) => state.serverError
+);
+
+export const getAuthData = createSelector(
+  getFeature,
+  (state) => state.authData
+);
+
+export const getAccessToken = createSelector(
+  getAuthData,
+  (authData) => authData?.accessToken
+);
+
+export const isAuth = createSelector(
+  getAccessToken,
+  (accessToken) => !! accessToken
+);
