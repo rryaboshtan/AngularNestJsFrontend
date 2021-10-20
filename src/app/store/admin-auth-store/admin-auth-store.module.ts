4d36eff58a52c8dde1366239ea12e408a08f1c0b
rryaboshtan <rrvworkroman@gmail.com>
2021-10-20 22:55:39 +0300

    Add auth service for immediately token get

--- src/app/store/admin-auth-store/admin-auth-store.module.ts
+++ src/app/store/admin-auth-store/admin-auth-store.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { adminAuthReducer, ADMIN_AUTH_FEATURE_NAME } from './store/admin-auth.reducer';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    HttpClientModule,
     StoreModule.forFeature(ADMIN_AUTH_FEATURE_NAME, adminAuthReducer),
   ],
 })
