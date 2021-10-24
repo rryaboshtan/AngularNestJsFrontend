673df66f6ad731df9aaa01787de7ef9058f26991
rryaboshtan <rrvworkroman@gmail.com>
2021-10-24 04:35:20 +0300

    create refresh effect

--- src/app/store/admin-auth-store/store/admin-auth.effects.ts
+++ src/app/store/admin-auth-store/store/admin-auth.effects.ts
@@ -1,29 +1,52 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType} from '@ngrx/effects';
-import { catchError, map, switchMap} from 'rxjs/operators';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, switchMap, delay } from 'rxjs/operators';
 import { AdminAuthService } from '../services/admin-auth.service';
 import { login, loginFailed, loginSuccess } from './admin-auth.actions';
 import { of } from 'rxjs';
+import { AuthData } from './admin-auth.reducer';
 
 @Injectable()
 export class AdminAuthEffects {
   login$ = createEffect(() =>
     this.actions$.pipe(
       ofType(login),
-        switchMap((action) => this.adminAuthService.login({
+      switchMap((action) =>
+        this.adminAuthService
+          .login({
             username: action.login,
             password: action.password,
-        }).pipe(
-            map(loginSuccessData => loginSuccess(loginSuccessData)),
-            catchError(error => of(loginFailed({
-                serverError: error.message   
-            }))
+          })
+          .pipe(
+            map((loginSuccessData: AuthData) => loginSuccess(loginSuccessData)),
+            catchError((error) =>
+              of(
+                loginFailed({
+                  serverError: error.message,
+                })
+              )
             )
-        ))
-    ));
+          )
+      )
+    )
+  );
 
-    constructor(
-        private actions$: Actions,
-        private adminAuthService: AdminAuthService,
-    ) { }
+  refresh$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loginSuccess),
+      delay(5000),
+      switchMap(() =>
+        this.adminAuthService
+          .refresh()
+          .pipe(
+            map((loginSuccessData: AuthData) => loginSuccess(loginSuccessData))
+          )
+      )
+    )
+  );
+
+  constructor(
+    private actions$: Actions,
+    private adminAuthService: AdminAuthService
+  ) {}
 }
